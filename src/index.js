f2b615a016d957563573e1ac6903cc3720620f86
jarifMohammed <mohammedjarif@protonmail.com>
2025-02-02 21:34:01 +0600

    Client getting message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 const path =require('path')
+const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
+
+
 const app =express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port =process.env.PORT || 3000
 
@@ -9,6 +15,18 @@ const publicPath=path.join(__dirname, '../public')
 
 app.use(express.static(publicPath))
 
-app.listen(port, ()=>{
+io.on('connection', (socket)=>{
+    console.log('mew websocket connection');
+
+    socket.emit('message', 'Welcome!')
+
+    socket.on('sendMessage', (message) => {
+
+        io.emit('message',message)
+    })
+
+})
+
+server.listen(port, ()=>{
     console.log(`Server is running at port ${port}`);
 })
