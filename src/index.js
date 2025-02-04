8ffab519289e46746501530115d18d7e0b70210a
jarifMohammed <mohammedjarif@protonmail.com>
2025-02-04 07:22:33 +0600

    Form button states

--- src/index.js
+++ src/index.js
@@ -31,11 +31,12 @@ io.on("connection", (socket) => {
     callback();
   });
 
-  socket.on("sendLocation", (cords) => {
+  socket.on("sendLocation", (cords, callback) => {
     io.emit(
       "message",
       `https://google.com/maps?q=${cords.latitude},${cords.longitude}`
     );
+    callback()
   });
   socket.on("disconnect", () => {
     io.emit("message", "A user has left");
